c7c4325dd9f75d692f2e2417be1610a3013705d2
WIlysihoming <willysihombing5@gmail.com>
2022-07-09 19:23:55 +0800

    remove autehnticator on register

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../controllers/user_controller");
 const authenticator = require("../middlewares/authentication");
 
-router.post("/users", authenticator, userController.registerUser);
+router.post("/users", userController.registerUser);
 router.get("/users", authenticator, userController.getAllUser);
 router.post("/users/login", userController.loginUser);
 router.patch("/users/approve", authenticator, userController.approve_user);
